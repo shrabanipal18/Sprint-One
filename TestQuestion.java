ab9790db1d6fb2da9eeae48a2d5f221a579ed456
shrabanipal18 <120494577+shrabanipal18@users.noreply.github.com>
2023-03-20 19:46:30 +0530

    Add files via upload

---
+++ TestQuestion.java
@@ -0,0 +1,24 @@
+package oops;
+import java.util.*;
+
+public class TestQuestion {
+
+public static void main(String[] args) {
+
+List<String> student = new ArrayList<String>();
+
+student.add("Vikash");
+
+student.add("Suchita");
+
+String name = "Deepak";
+
+Object stdnt = new String("Shivam");
+
+student.add(name);
+
+//student.add(stdnt);
+
+}
+
+}
